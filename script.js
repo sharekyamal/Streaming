9daeb60656535ba708c1667bc12cbaece34f963b
sharekyamal <sharekyamal@gmail.com>
2025-04-13 01:07:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 // Sample video data (used in both index.html and video.html)
 let videos = {
   live1: {
-    embedCode: '<iframe src="https://www.facebook.com/plugins/video.php?href=YOUR_LIVE_VIDEO_URL" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true"></iframe>',
+    embedCode: '<iframe src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fweb.facebook.com%2Fdnaindia%2Fvideos%2F1000703981665667%2F&show_text=false&width=560&t=0" width="560" height="314" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>',
     title: 'SBO Live',
     streamer: 'မိုး',
+    thumbnail: 'https://via.placeholder.com/120x80?text=Live+Stream+1',
+    viewers: 'Live Now',
+    isLive: true,
+  },
+  live2: {
+    embedCode: '<iframe src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fweb.facebook.com%2Fdnaindia%2Fvideos%2F1000703981665667%2F&show_text=false&width=560&t=0" width="560" height="314" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>',
+    title: 'SBO Live',
+    streamer: 'ကတုံး',
     thumbnail: 'https://via.placeholder.com/120x80?text=Live+Stream+1',
     viewers: 'Live Now',
     isLive: true,
